3b8ad4ecee53b18435a53323f3ab394d19330fb2
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-27 19:47:51 -0500

    have front-end set up to show articles if the array has length, show other options otherwise.

---
+++ public/assets/javascript/front-end.js
@@ -0,0 +1,23 @@
+var dummyArticles = [{"article_title": "article title","article_link": "article link","article_comment_collection": [{"article_comment": "I'm a comment"}]},{"article_title": "article title 2","article_link": "article link 2","article_comment_collection": [{"article_comment": "I'm another comment"}]}];
+// var dummyArticles = [];
+
+$(document).ready(function(){
+    console.log("javascript running")
+    console.log(dummyArticles.length);
+    
+    if (dummyArticles.length) {
+        $("#no-articles").addClass("hidden");
+        $("#articles").removeClass("hidden");
+        for (var i = 0; i < dummyArticles.length; i++) {
+            console.log(dummyArticles[i]);
+            var title = dummyArticles[i].article_title;
+            var link = dummyArticles[i].article_link;
+            $("#articles").append(`<p>${title}</p><p>${link}</p>`);
+        };
+
+    } else {
+        $("#no-articles").removeClass("hidden");
+        $("#articles").addClass("hidden");
+    };
+
+})
