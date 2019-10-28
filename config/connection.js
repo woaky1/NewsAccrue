8d978245b2190527427cb4acd25098e7cee746a4
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-27 19:13:51 -0500

    not quite sure yet how to hook up mongodb with the application. Gonna move back to front end anwork off of dummy data.

---
+++ config/connection.js
@@ -0,0 +1,4 @@
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsaccrue";
+
+mongoose.connect(MONGODB_URI);
