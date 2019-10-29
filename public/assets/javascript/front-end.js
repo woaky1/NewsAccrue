62a49b5ec57e49ccd50c98e6ac11a681d7b7ba3f
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-29 15:56:02 -0500

    I hate how this is working. Committing, then starting a new branch and going to try again.

--- public/assets/javascript/front-end.js
+++ public/assets/javascript/front-end.js
@@ -1,23 +1,27 @@
-// var dummyArticles = [{"article_title": "article title","article_link": "article link", "article_saved": false, "article_comment_collection": [{"article_comment": "I'm a comment"}]},{"article_title": "article title 2","article_link": "article link 2", "article_saved": false, "article_comment_collection": [{"article_comment": "I'm another comment"}]}];
-var dummyArticles = [];
 
 $(document).ready(function(){
     console.log("javascript running")
-    console.log(dummyArticles.length);
-    
-    if (dummyArticles.length) {
-        $("#no-articles").addClass("hidden");
-        $("#articles").removeClass("hidden");
-        for (var i = 0; i < dummyArticles.length; i++) {
-            console.log(dummyArticles[i]);
-            var title = dummyArticles[i].article_title;
-            var link = dummyArticles[i].article_link;
-            $("#articles").append(`<p>${title}</p><p>${link}</p>`);
-        };
-
-    } else {
-        $("#no-articles").removeClass("hidden");
-        $("#articles").addClass("hidden");
-    };
-
-})
+    $.ajax({
+        url: "/api",
+        method: "GET"
+      }).then (
+        function(response) {
+            if (response.length) {
+                $("#no-articles").addClass("hidden");
+                $("#articles").removeClass("hidden");
+                for (var i = 0; i < response.length; i++) {
+                    console.log(response[i]);
+                    var title = response[i].article_title;
+                    var link = response[i].article_link;
+                    $("#articles").append(`<p>${title}</p><p>${link}</p>`);
+                };
+        
+            } else {
+                $("#no-articles").removeClass("hidden");
+                $("#articles").addClass("hidden");
+            };
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+});
